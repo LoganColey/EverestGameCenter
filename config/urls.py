d93ae2aed4c53192d7da445dd94b409f0bfe4625
LoganColey <lcoley22@basecampcodingacademy.org>
2021-12-16 15:44:58 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,5 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('snake/', views.snake, name="snake"),
-    path('flappy_birb/', views.flappy_birb)
+    path('flappybirb/', views.flappy_birb)
 ]
