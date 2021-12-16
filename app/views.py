750e1ff835819df29013551059ac055dd63fa958
LoganColey <lcoley22@basecampcodingacademy.org>
2021-12-16 09:47:15 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def flappy_birb(request) -> HttpResponse:
+    return render(request, 'flabbybirb.html')
+
+def snake(request) -> HttpResponse:
+    return render(request, 'snake.html')
