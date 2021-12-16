85f45a144caf586adae46c8d4d34a39cb4e363af
LoganColey <lcoley22@basecampcodingacademy.org>
2021-12-16 09:47:19 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('snake/', views.snake, name="snake"),
+    path('flappy_birb/', views.flappy_birb)
 ]
