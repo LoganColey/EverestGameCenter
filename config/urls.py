dc142ff92f95926da885cac1fcc9c8b25326bccf
LoganColey <lcoley22@basecampcodingacademy.org>
2021-12-28 14:30:58 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,8 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('snake/', views.snake, name="snake"),
-    path('flappybirb/', views.flappy_birb)
+    path('flappybirb/', views.flappy_birb),
+    path('spaceinvaders/', views.space_invaders),
+    path('run/', views.run),
+    path('', views.main)
 ]
