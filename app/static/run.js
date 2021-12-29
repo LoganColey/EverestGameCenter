5ddb6a020c4ffb2735e96040a29b5f6b313a0246
LoganColey <lcoley22@basecampcodingacademy.org>
2021-12-29 09:32:13 -0600

    Update run.js

--- app/static/run.js
+++ app/static/run.js
@@ -6,7 +6,7 @@ function jump(){
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
-    },300);
+    },500);
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
