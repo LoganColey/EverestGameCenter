d221f2e30b5974611034fe5e4b029784de94cb30
LoganColey <lcoley22@basecampcodingacademy.org>
2021-12-30 13:54:38 -0600

    Update run.js

--- app/static/run.js
+++ app/static/run.js
@@ -6,12 +6,12 @@ function jump(){
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
-    },500);
+    },300);
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=400){
         block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
         counter=0;
