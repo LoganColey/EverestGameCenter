9507a7b55c22ed5ba411be51ee6c5712139d161e
LoganColey <lcoley22@basecampcodingacademy.org>
2021-12-30 13:54:28 -0600

    Update main.js

--- app/static/main.js
+++ app/static/main.js
@@ -17,17 +17,20 @@
 //     myIndex += n;
 //   }
 var myIndex = 0;
-var x = document.getElementsByClassName("mySlides");
 carousel(1);
 
 function carousel(n) {
     clearInterval(myTimer)
+    auto(n);
+    function auto(f){
+    var x = document.getElementsByClassName("mySlides");
     for (var i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    myIndex += n;
+    myIndex += f;
     if (myIndex > x.length) {myIndex = 1}
     if (myIndex < 1) {myIndex = x.length}
     x[myIndex - 1].style.display = "block"
-    var myTimer = setInterval(carousel(1), 1000);
+}
+    var myTimer = setInterval(auto(1), 5000);
 }
