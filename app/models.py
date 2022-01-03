3fd525d14012e6ba2708beb50a8342b8dfd52654
LoganColey <lcoley22@basecampcodingacademy.org>
2022-01-03 13:11:19 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20, null=True)
+    password = models.CharField(max_length=15, null=True)
+    pac_high_score = models.PositiveIntegerField(null=True)
+    snake_high_score = models.PositiveIntegerField(null=True)
+    run_high_score = models.PositiveIntegerField(null=True)
+    flap_high_score = models.PositiveIntegerField(null=True)
+    galaga_high_score = models.PositiveIntegerField(null=True)
+    numbers_high_score = models.PositiveIntegerField(null=True)
+    tetris_high_score = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+        
+class Achievement(models.Model):
+    game = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=100, null=True)
+    user = models.ManyToManyField(User)
