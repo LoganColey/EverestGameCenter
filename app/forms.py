82e1f7f11c9820eca5d0d990925e187d1caf1cbe
LoganColey <lcoley22@basecampcodingacademy.org>
2022-01-05 08:14:40 -0600

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from app.models import  *
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username...', 'style':'width: 300px; color: black;'}), max_length=20, required=True)
-    password = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Password...', 'style':'width: 300px; color: black;'}), max_length=15, required=True)
+class LoginForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
 
 
-class RegisterForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username...', 'style':'width: 300px; color: black;'}), max_length=20, required=True)
-    password1 = forms.CharField(label="Password", widget=forms.TextInput(attrs={'placeholder':'Password...', 'style':'width: 300px; color: black;'}), max_length=15, required=True)
-    password2 = forms.CharField(label="Repeat Password", widget=forms.TextInput(attrs={'placeholder':'Repeat Password...', 'style':'width: 300px; color: black;'}), max_length=15, required=True)
-    # class Meta:
-    #     model = User
-    #     fields = ['username', 'password1', 'password2']
