313fa4466e39177f34ec1e3874635303721fa5b4
LoganColey <lcoley22@basecampcodingacademy.org>
2022-01-05 08:15:20 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,11 +6,12 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('snake/', views.snake, name="snake"),
-    path('flappybirb/', views.flappy_birb),
-    path('spaceinvaders/', views.space_invaders),
-    path('run/', views.run),
+    path('flappybirb/', views.flappy_birb, name="flappybirb"),
+    path('spaceinvaders/', views.space_invaders, name="galaga"),
+    path('run/', views.run, name="run"),
     path('', views.main, name="main"),
     path('login/', views.login_page, name="login"),
     path('signup/', views.signup, name="signup"),
-    path('pacman/', views.pacman)
+    path('pacman/', views.pacman, name="pacman"),
+    path("logout/", views.logoutUser, name="logout")
 ]
